68903d63e826d94dcb6f6f9893449b9ca2e21376
Professorarafaela <125619045+Professorarafaela@users.noreply.github.com>
2024-10-30 18:56:14 -0300

    alt

--- graficos/informacoesglobais.js
+++ graficos/informacoesglobais.js
@@ -4,5 +4,11 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados)
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o total de pessoas no mundo é de ${dados.total_pessoas_mundo} de pessoas e que o  total de pessoas com acesso à educação é de ${dados.total_pessoas_com_acesso_a_educacao}? O tempo médio gasto por dia estudando é de ${dados.tempo_medio_dia_estudando} horas e o total de pessoas com educação superior é de ${dados.total_pessoas_com_educacao_superior}.  Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?
+`
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo);
 }
 vizualizarInformacoesGlobais()
