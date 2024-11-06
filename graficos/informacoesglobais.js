1796bcc48ec6642e61f6c5c277a36fc448fa6b1d
Professorarafaela <125619045+Professorarafaela@users.noreply.github.com>
2024-11-06 19:15:04 -0300

    alt

--- graficos/informacoesglobais.js
+++ graficos/informacoesglobais.js
@@ -3,11 +3,16 @@ const url='https:// https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados)
+    //console.log(dados)
+    const pessoasConectadas = (dados.total_trabalho / 1e9)
+const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+const horas = parseInt(dados.tempo_medio)
+const minutos = Math.round((dados.tempo_medio - horas) * 60)
+
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-     paragrafo.innerHTML = `Você sabia que o total de pessoas no mundo é de <span>${dados.total_pessoas_mundo}</span> de pessoas e que o  total de pessoas com acesso à educação é de <span>${dados.total_pessoas_com_acesso_a_educacao}?</span> O tempo médio gasto por dia estudando é de <span>${dados.tempo_medio_dia_estudando} horas </span> e o total de pessoas com educação superior é de <span>${dados.total_pessoas_com_educacao_superior}</span>. Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?
-`
+     paragrafo.innerHTML = ` O trabalho é muito importante para o ser humano, tanto que de um total de <span>${dados.total_pessoas_do_mundo}?</span> apenas  <span>${dados.total_desempregados} </span> de desempregados e o <span>${dados.total_empregados}</span>, da população em idade ativa. Para o tempo médio de trabalho temos um total de <span>$ {dados.tempo_de_trabalho_semanal?</span> . E você, trabalha? Quantas horas por semana? Acredita que isso é importante, porque?`
+
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
